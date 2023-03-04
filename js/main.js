99accb7d352d4de4d5a023ab65a94c46ec894e59
retardH <loowmaad@gmail.com>
2023-03-04 11:05:05 +0630

    Changed main header font size

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ let _html = document.querySelector('html');
 let toggle = document.querySelector('.toggle');
 let toggleCheckbox = document.querySelector('.toggle__checkbox');
 
-if(!toggleCheckbox.checked) {
-    _html.classList.remove('dark');
+function toggleTheme() {
+    if(toggleCheckbox.checked) {
+        _html.classList.add('dark');
+    } else if(!toggleCheckbox.checked) {
+        _html.classList.remove('dark');
+    }
 }
 
-toggleCheckbox.addEventListener('click', () => {
-    _html.classList.toggle('dark');
-})
+toggleCheckbox.addEventListener('click',toggleTheme);
 
 
